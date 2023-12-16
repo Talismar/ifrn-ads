6deb17386f750b0bc955484863e80c3e1e7f2968
Talismar Fernandes Costa <talismar788.una@gmail.com>
2023-12-15 21:48:48 -0300

    Finished activity

---
+++ README.md
@@ -0,0 +1,7 @@
+## Descrição da atividade
+
+Pensando na problemática do jantar dos filósofos escreva códigos para solucionar o problema baseado nessas hipóteses de solução:
+
+1. Solução de Limitação de Acessos
+2. Solução quebrando o Hold-and-wait
+3. Solução através da imposição de ordem
---
+++ solution_two.py
@@ -0,0 +1,36 @@
+import threading
+
+
+class Filosofo:
+    def __init__(self, nome, garfo_esquerdo, garfo_direito):
+        self.nome = nome
+        self.garfo_esquerdo = garfo_esquerdo
+        self.garfo_direito = garfo_direito
+
+    def comer(self):
+        with self.garfo_esquerdo:
+            with self.garfo_direito:
+                print(f"{self.nome} está comendo.")
+
+
+def jantar_dos_filosofos_quebrando_hold_and_wait():
+    garfos = [threading.Lock() for _ in range(5)]
+    threads = []
+
+    for i in range(5):
+        nome = f"Filósofo {i}"
+        garfo_esquerdo = garfos[i]
+        garfo_direito = garfos[(i + 1) % 5]
+
+        filosofo = Filosofo(nome, garfo_esquerdo, garfo_direito)
+
+        thread = threading.Thread(target=filosofo.comer)
+        threads.append(thread)
+
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+
+jantar_dos_filosofos_quebrando_hold_and_wait()
